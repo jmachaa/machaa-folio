2eb7a4c8948bc9393dcae7800bd47c1a4d9273dd
jmachaa <150440355+jmachaa@users.noreply.github.com>
2025-05-02 20:56:21 +0000

    final

--- macha-folio/src/components/Home.js
+++ macha-folio/src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
         "JavaScript",
         "Laravel",
         "PHP",
-        "Tailwind CSS",
+        "MongoDB",
         "MySQL",
         "REST APIs",
         "Git & GitHub",
@@ -14,8 +14,10 @@ const Home = () => {
     return (
       <div className="min-h-screen">
       <header>
+      <img src="https://avatars.githubusercontent.com/u/9919?s=200&v=4" alt="Thomas P Y" className="profile-image" />
         <h1>Thomas P Y</h1>
-        <p>React & Laravel Developer</p>
+        <p className='react-p'>React & Laravel Developer</p>
+        <p className='header-p'>I’m a Full-Stack Developer with 2.5 years of experience building responsive, scalable web applications using React and Laravel. I focus on clean code, performance, and delivering real value through intuitive user experiences. I’ve worked on dynamic projects like loanclaims.unfairfees, streamlining user data flow and backend operations. Always curious, always improving.</p>
       </header>
 
       <section className="skills-section">
@@ -30,16 +32,17 @@ const Home = () => {
       <section className="projects-section">
         <h2>Projects</h2>
         <div className="project-card">
-          <h3>Loan Claims Platform</h3>
+          {/* <h3>Loan Claims Platform</h3>
           <p>React + Laravel app to collect user data and assist clients with claims.</p>
-          <a href="https://loanclaims.unfairfees.com" target="_blank" rel="noopener noreferrer">View Project →</a>
+          <a href="https://loanclaims.unfairfees.com" target="_blank" rel="noopener noreferrer">View Project →</a> */}
+          Yet to be added...
         </div>
       </section>
 
       <section className="contact-section">
         <h2>Get In Touch</h2>
         <p>I’m open to freelance, full-time opportunities, or collaborations.</p>
-        <a href="mailto:your-email@example.com">Contact Me</a>
+        <a href="mailto:thomaspy98@gmail.com">Contact Me</a>
       </section>
 
       <footer>
